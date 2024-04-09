ba11731154ffd826e1ba197de0f0f12ced520481
ahoang928 <ahoang928@gmail.com>
2024-04-08 21:25:37 -0400

    navbar stuff

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './navbar.css'
+import logo from '../../assets/cat.png'
+import { Link } from 'react-scroll'
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className='navbar'>
+        <img src={logo} alt='Logo' className='logo'/>
+        <div className='desktopMenu'>
+            <Link className='desktopMenuListItem'>Home</Link>
+            <Link className='desktopMenuListItem'>About Me</Link>
+            <Link className='desktopMenuListItem'>Portfolio</Link>
+        </div>
+        <button className='desktopMenuBtn'>
+            <img src='' alt='' className='desktopMenuImg'/>Contact Me
+        </button>
+    </nav>
   )
 }
 
-export default navbar
+export default Navbar
